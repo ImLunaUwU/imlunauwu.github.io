ef291dc57c30bf102fb91f967f6fdd5520aaf795
Luna <jeppek04@gmail.com>
2023-02-24 02:17:41 +0100

    djkhgdskjgh

--- assets/js/loadingscript.js
+++ assets/js/loadingscript.js
@@ -1,26 +1,22 @@
 window.onload = function () {
-		console.log("What are you doin´ in me swamp!?");
-		console.log("If you read this, you're gay.");
-		setTimeout(function(){
+	setTimeout(function(){
     document.getElementById("loading").id = 'loaded';
 	document.getElementById("bodyloading").id = 'bodyloaded';
     console.log("Loading Complete!");
     }, 500);
+
 	// Remove Overlay After Load
-		console.log("Loading gun.");
-		setTimeout(function(){
+	setTimeout(function(){
     var el = document.getElementById('finishedloading');
 	el.remove();
+
 	// Fade-in body when the page has loaded
 	$("body").fadeIn(2000);
-    console.log("*kills load overlay*");
 	}, 1100);
 
 	// Remove Load Script After Load
-		console.log("Loading 2nd gun.");
-		setTimeout(function(){
-    var el = document.getElementById('suicidaljavascript');
-el.remove();
-    console.log("*commits die*");
+	setTimeout(function(){
+    var el = document.getElementById('selfdelete');
+	el.remove();
 	}, 1200);
 }
